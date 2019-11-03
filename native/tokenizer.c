3708ec66820075a518bc3dce93713d4b61307ec3
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-11-03 07:29:52 +0100

    Splitting source into lines in minic tokenizer

--- native/tokenizer.c
+++ native/tokenizer.c
@@ -60,12 +60,17 @@ struct StringList * collect_lines(const struct String *text, struct StringList *
     const char *iterator = text->value;
     const char *line_start = text->value;
     const char *end = text->value + text->length;
+    int current_line_processed = 0;
     while (iterator != end) {
         if (*iterator == '\r' || *iterator == '\n') {
-            lines = prepend_line(lines, iterator, line_start);
+            if (!current_line_processed) {
+                lines = prepend_line(lines, iterator, line_start);
+                current_line_processed = 1;
+            }
         }
         if (*iterator == '\n') {
             line_start = iterator + 1;
+            current_line_processed = 0;
         }
         ++iterator;
     }
@@ -79,25 +84,26 @@ static struct Source *create_source(struct String *text) {
     struct StringList *lines = StringList_create();
     struct StringList *const last = lines;
     lines = collect_lines(text, lines);
+    const size_t size = lines->size;
     
-    struct String *result_lines = allocate(sizeof(struct String) * lines->size);
+    struct String *result_lines = allocate(sizeof(struct String) * size);
     struct String *result_line_iterator = result_lines;
     for (struct StringList *current = last->previous; current != 0; current = current->previous) {
-        if (result_line_iterator - result_lines >= lines->size) {
+        if (result_line_iterator - result_lines >= size) {
             fail("Logical error in result lines creation");
         }
         *result_line_iterator = *current->value;
         ++result_line_iterator;
     }
-    if (result_line_iterator - result_lines != lines->size) {
+    if (result_line_iterator - result_lines != size) {
         fail("Logical error in result lines creation - offset: [%zu], size: [%zu]",
-                result_line_iterator - result_lines, lines->size);
+                result_line_iterator - result_lines, size);
     }
     
     StringList_free(lines);
     
     struct Source *result = allocate(sizeof(struct Source));
-    result->number_of_lines = lines->size;
+    result->number_of_lines = size;
     result->lines = result_lines;
     return result;
 }
