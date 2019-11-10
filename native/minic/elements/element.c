634b80bc0636bd69b00cf02312a0165d9cb3e2ad
Sebastian Bohmann <mail@sebastian-bohmann.com>
2019-11-10 12:39:55 +0100

    Element list creation

--- native/minic/elements/element.c
+++ native/minic/elements/element.c
@@ -1,9 +1,27 @@
 #include "element.h"
 
+#include <string.h>
+
 #include <core/allocate.h>
 #include <minic/tokens/token_queue.h>
 
 #include "generated/element_list.c"
+
+bool equal(const struct String *lhs, const char *rhs) {
+    return strcmp(lhs->value, rhs) == 0;
+}
+
+enum BracketType bracket_type(const struct String *text) {
+    if (equal(text, "(") || equal(text, ")")) {
+        return Paren;
+    } else if (equal(text, "[") || equal(text, "]")) {
+        return Paren;
+    } else if (equal(text, "{") || equal(text, "}")) {
+        return Paren;
+    } else {
+        fail("Not a bracket [%s]", text->value);
+    }
+}
 
 struct Element *token_element(const struct Token *token) {
     struct Element *result = allocate(sizeof(struct Element());
@@ -12,13 +30,38 @@ struct Element *token_element(const struct Token *token) {
     return result;
 }
 
-void add_bracket(struct ElementList *elements, struct TokenQueue *tokens, const struct String *openingBracket) {
-    struct ElementList *bracket_elements = collect_elements(queue);
-    struct Elements *result = allocate(sizeof(struct Elements));
+static struct Elements flatten(struct ElementList *elements) {
+    struct Elements result;
+    memset(&result, 0, sizeof(struct Elements));
+    result.data = allocate(sizeof(struct Element) * elements->size);
+    result.size = 0;
+    struct ElementListElement *iterator = ElementList_begin();
+    while (iterator) {
+        struct Element *element = ElementListIterator_get(iterator);
+        if (result.size == elements->size) {
+            fail("Overrun in flat element list creation");
+        }
+        result.data[result.size] = element;
+    }
+    if (result.size != elements->size) {
+        fail("Underrun in flat element list creation");
+    }
+    ElementList_delete(elements);
     return result;
 }
 
-void add_elements(struct ElementList *elements, struct TokenQueue *tokens) {
+static struct ElementList *collect_elements(struct TokenQueue *tokens);
+
+void add_bracket(struct ElementList *elements, struct TokenQueue *tokens, const struct String *openingBracket) {
+    struct ElementList *bracket_elements = collect_elements(tokens);
+    struct Element *result = allocate(sizeof(struct BracketElement));
+    result->type = BracketElement;
+    result->bracket.type = bracket_type(openingBracket);
+    result->bracket.elements = flatten(bracket_elements);
+    ElementList_append(elements, result);
+}
+
+static void add_elements(struct ElementList *elements, struct TokenQueue *tokens) {
     while (true) {
         const struct Token *token = TokenQueue_peek(tokens);
         if (token) {
@@ -37,7 +80,7 @@ void add_elements(struct ElementList *elements, struct TokenQueue *tokens) {
     }
 }
 
-struct ElementList *collect_elements(struct TokenQueue *tokens) {
+static struct ElementList *collect_elements(struct TokenQueue *tokens) {
     struct ElementList *result = ElementList_create();
     add_elements(result, tokens);
     return result;
@@ -47,19 +90,6 @@ struct Elements *read_elements(struct Tokens *tokens) {
     struct TokenQueue *queue = TokenQueue_create(tokens);
     struct ElementList *elements = collect_elements(queue);
     struct Elements *result = allocate(sizeof(struct Elements));
-    result->data = allocate(sizeof(struct Element) * elements->size);
-    result->size = 0;
-    struct ElementListElement *iterator = ElementList_begin();
-    while (iterator) {
-        struct Element *element = ElementListIterator_get(iterator);
-        if (result.size == elements->size) {
-            fail("Overrun in flat element list creation");
-        }
-        result.data
-    }
-    if (result.size != elements->size) {
-        fail("Underrun in flat element list creation");
-    }
-    ElementList_delete(elements);
+    *result = flatten(elements);
     return result;
 }
