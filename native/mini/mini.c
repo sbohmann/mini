dc680a4affffb133ffd232e4b17586b0cf9eda6b
Sebastian Bohmann <mail@sebastian-bohmann.com>
2020-03-01 09:34:53 +0100

    Preparing the parser GC

--- native/mini/mini.c
+++ native/mini/mini.c
@@ -1,11 +1,12 @@
-#include <minic/module/module.h>
+#include <stdio.h>
 
 #include <core/errors.h>
-
+#include <minic/module/module.h>
 
 int main(int argc, const char **argv) {
     if (argc != 2) {
         fail_with_message("Expecting single argument <path to source file>");
     }
     struct ParsedModule *module = ParsedModule_read(argv[1]);
+    printf("OK.")
 }
