b54566fa8d6e3695c4ae31edc14f5027e425289e
sbohmann <mail@sebastian-bohmann.com>
2023-02-10 21:35:41 +0100

    whitespace because bbedit

--- bootstrap/configuration.h
+++ bootstrap/configuration.h
@@ -1,14 +1,14 @@
 #include <string.h>
 
-static const char *signature = "@_";
-static const size_t signature_length = sizeof(signature) - 1;
+static const char *start_signature = "@_";
+static const size_t start_signatur_length = sizeof(signature) - 1;
 
-static const char *end = ";";
-static const size_t end_length = sizeof(end) - 1;
+static const char *end_signature = ";";
+static const size_t end__signatur_length = sizeof(end) - 1;
 
 static bool is_name_part(c) {
-	return c == '_' ||
-		(c >= 'A' && c <= 'Z') ||
-		(c >= 'a' && c <= 'z') ||
-		(c >= '0' && c <= '9');
+    return c == '_' ||
+        (c >= 'A' && c <= 'Z') ||
+        (c >= 'a' && c <= 'z') ||
+        (c >= '0' && c <= '9');
 }
--- bootstrap/main.c
+++ bootstrap/main.c
@@ -7,10 +7,10 @@
 #include "configuration.h"
 
 enum State {
-	Verbatim,
-	StartSignature,
-	Name,
-	EndSignature
+    Verbatim,
+    StartSignature,
+    Name,
+    EndSignature
 };
 
 enum State state;
@@ -19,7 +19,7 @@ const size_t buffer_size = 256;
 char buffer[buffer_size];
 
 struct {
-	size_t offset;
+    size_t offset;
 } signature_state;
 
 struct {
@@ -29,33 +29,37 @@ struct {
 } end_state;
 
 static void init() {
-	state = Verbatim;
+    state = Verbatim;
 }
 
 static void process(char c) {
-	switch (state) {
-		case Verbatim:
-			if (c == signature[0]) {
-				state = StartSignature;
-				signature_state.offset = 0;
-			} else {
-				putchar(c);
-			}
-	}
+    switch (state) {
+        case Verbatim:
+            if (c == signature[0]) {
+                state = StartSignature;
+                signature_state.offset = 1;
+            } else {
+                putchar(c);
+            }
+        case StartSignature:
+            if (signature_state.offset <= start_signature_length) {
+                
+            }
+    }
 }
 
 int main() {
-	init();
-	while (true) {
-		char c = getchar();
-		if (c < 0) {
-			if (errno != 0) {
-				fprintf(stderr, "Error while reading input: %s\n", strerror(errno));
-				exit(1);
-			} else {
-				break;
-			}
-		}
-		process(c);
-	}
+    init();
+    while (true) {
+        char c = getchar();
+        if (c < 0) {
+            if (errno != 0) {
+                fprintf(stderr, "Error while reading input: %s\n", strerror(errno));
+                exit(1);
+            } else {
+                break;
+            }
+        }
+        process(c);
+    }
 }
