0b1f630e4ab1d39a8bf0fe9e22b02fde0727fea8
sbohmann <mail@sebastian-bohmann.com>
2023-02-11 09:07:17 +0100

    States

--- bootstrap/main.c
+++ bootstrap/main.c
@@ -4,21 +4,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-#include "configuration.h"
-
-enum State {
-    Verbatim,
-    StartSignature,
-    Name,
-    EndSignature
-};
-
-enum State state;
-
-struct {
-    size_t offset;
-} signature_state;
-
 struct {
     char buffer[name_buffer_capacity];
     size_t size;
@@ -27,38 +12,8 @@ struct {
 struct {
 } end_state;
 
-static void init() {
-    state = Verbatim;
-}
-
-static void process(char c) {
-    switch (state) {
-        case Verbatim:
-            if (c == start_signature[0]) {
-                signature_state.offset = 1;
-                state = StartSignature;
-            } else {
-                putchar(c);
-            }
-            break;
-        case StartSignature:
-            if (signature_state.offset < start_signature_length - 1) {
-                if (c == start_signature[signature_state.offset]) {
-                    ++signature_state.offset;
-                } else {
-                    // TODO write signature to offset as verbatim
-                    // and set state to Verbatim
-                }
-            } else {
-                name_state.buffer[0] = c;
-                name_state.size = 1;
-                state = Name;
-            }
-    }
-}
-
 int main() {
-    init();
+    initialize();
     while (true) {
         char c = getchar();
         if (c < 0) {
